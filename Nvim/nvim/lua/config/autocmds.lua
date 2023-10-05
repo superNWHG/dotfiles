5f84b91c3912b0c2414e19d778478b0555224968
superNWHG <geesinknico@gmail.com>
2023-10-05 20:17:16 +0200

    Changed nvim completion to switch between the completions with tab

--- Nvim/nvim/lua/config/autocmds.lua
+++ Nvim/nvim/lua/config/autocmds.lua
@@ -1,3 +1,4 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- This file is automatically loaded by lazyvim.config.init.
