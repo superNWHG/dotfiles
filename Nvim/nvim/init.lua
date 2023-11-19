a0d7971676fb7e52669f97b7b3534ee04c1209f7
superNWHG <geesinknico@gmail.com>
2023-11-19 15:49:31 +0100

    Changed nvim config to use underline cursor

--- Nvim/nvim/init.lua
+++ Nvim/nvim/init.lua
@@ -1,3 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("set nornu!")
+vim.cmd([[
+    augroup change_cursor
+        au!
+        au ExitPre * :set guicursor=a:hor25
+    augroup END
+]])
---
+++ Nvim/nvim/lua/plugins/lf.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "ptzz/lf.vim",
+  },
+}
