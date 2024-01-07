cc463d33dcff6d75ddc96cc6388776c25e1d650e
superNWHG <geesinknico@gmail.com>
2024-01-07 19:47:17 +0100

    Improved the hyprland install script

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -2,11 +2,7 @@
 # Install script for the dotfiles
 
 # Create a debug.log
-sudo touch ~/dotfiles/debug.log/
-
-# Install Arduino theme
-sudo mv /usr/share/arduino/lib/theme/ /usr/share/arduino/lib/theme.bak/ >~/dotfiles/debug.log/
-sudo ln -s ~/dotfiles/Arduino/theme/ /usr/share/arduino/lib/ >~/dotfiles/debug.log/
+touch ~/dotfiles/debug.log/
 
 # Install betterdiscord
 sudo rm -rf ~/.config/BetterDiscord/ >~/dotfiles/debug.log/ >~/dotfiles/debug.log/
