9995d160ced776a9e7736355df2ab77a55a2be7f
Nico Geesink <geesinknico@gmail.com>
2024-10-27 12:45:22 +0100

    Removed an alias

--- Zsh/zsh/aliases.zsh
+++ Zsh/zsh/aliases.zsh
@@ -11,7 +11,6 @@ alias parrem="paru -Rns"
 alias parreps="paru -Ss"
 alias parmir="paru Syy"
 alias upgrade="paru -Syu"
-alias clean="paru -Scc && paru -Rns $(paru -Qtdq) && rm -rf ~/.cache/*"
 
 # General aliases
 alias ccd="cd && clear"
