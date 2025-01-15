101e9a0ba703795ee5e8c90132c0d9e661286feb
Nico Geesink <geesinknico@gmail.com>
2025-01-15 12:21:30 +0100

    Use URL

--- Scripts/scripts/mealie.sh
+++ Scripts/scripts/mealie.sh
@@ -4,6 +4,6 @@ credpath="$HOME/mealiecreds.json"
 url=$(jq -r ".url" $credpath)
 token=$(jq -r ".token" $credpath)
 date=$(date +"%Y-%m-%d")
-res=$(curl -s -X "GET" "https://mealie.geesink.org/api/households/mealplans?start_date=$date&end_date=$date&perPage=1" -H "accept: application/json" -H "Authorization: Bearer $token")
+res=$(curl -s -X "GET" "$url/api/households/mealplans?start_date=$date&end_date=$date&perPage=1" -H "accept: application/json" -H "Authorization: Bearer $token")
 
 echo $res | jq -r ".items[] | .recipe.name"
