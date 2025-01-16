05e9062a757108609397e4e5aeed7d884098d520
Nico Geesink <geesinknico@gmail.com>
2025-01-16 10:23:54 +0100

    Put mealie script in loop report every day

--- Scripts/scripts/mealie.sh
+++ Scripts/scripts/mealie.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-credpath="$HOME/mealiecreds.json"
-url=$(jq -r ".url" $credpath)
-token=$(jq -r ".token" $credpath)
-date=$(date +"%Y-%m-%d")
-res=$(curl -s -X "GET" "$url/api/households/mealplans?start_date=$date&end_date=$date&perPage=1" -H "accept: application/json" -H "Authorization: Bearer $token")
+while true; do
+	credpath="$HOME/mealiecreds.json"
+	url=$(jq -r ".url" $credpath)
+	token=$(jq -r ".token" $credpath)
+	date=$(date +"%Y-%m-%d")
+	res=$(curl -s -X "GET" "$url/api/households/mealplans?start_date=$date&end_date=$date&perPage=1" -H "accept: application/json" -H "Authorization: Bearer $token")
+	echo $res | jq -r ".items[] | .recipe.name"
 
-echo $res | jq -r ".items[] | .recipe.name"
+	sleep $(($(date -d "tomorrow 00:00" +"%s") - $(date +"%s")))
+done
